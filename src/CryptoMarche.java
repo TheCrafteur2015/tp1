c219c3f96a10c3bef9e5f792338a3b34c23133e9
martin <mabillemartin1@gmail.com>
2023-04-11 11:32:31 +0200

    lol

--- src/CryptoMarche.java
+++ src/CryptoMarche.java
@@ -5,44 +5,48 @@ public class CryptoMarche {
     private ArrayList<Portefeuille> portefeuilles;
     private static CryptoMarche marche;
 
-    private CryptoMarche(){
+    private CryptoMarche() {
         portefeuilles = new ArrayList<Portefeuille>();
     }
 
-    public static CryptoMarche getInstance(){
-        if(marche == null){ marche = new CryptoMarche();}
-		return marche;
+    public static CryptoMarche getInstance() {
+        if (marche == null) {
+            marche = new CryptoMarche();
+        }
+        return marche;
     }
 
-    public void ajouter(Portefeuille p){
+    public void ajouter(Portefeuille p) {
         portefeuilles.add(p);
     }
 
     /**
-     * Cette fonction recherche sur le marché tous les portefeuilles 
-     * du propriétaire et calcule son capital en euros. 
+     * Cette fonction recherche sur le marché tous les portefeuilles
+     * du propriétaire et calcule son capital en euros.
+     * 
      * @param proprietare
      * @return capital en euros du propriétare.
      */
-    public double capitalEnEuros(String proprietaire){
+    public double capitalEnEuros(String proprietaire) {
         /**
-			FONCTION À IMPLEMENTER
-        **/
+         * FONCTION À IMPLEMENTER
+         **/
 
         return 0;
     }
 
     /**
-     * Cette fonction recherche sur le marché tous les portefeuilles 
-     * d'un type de devise et calcule le volume total de capital de 
-     * cette devise sur le marché 
+     * Cette fonction recherche sur le marché tous les portefeuilles
+     * d'un type de devise et calcule le volume total de capital de
+     * cette devise sur le marché
+     * 
      * @param monnaie
      * @return capital total en circulation de la cryptomonnaie (en euros).
      */
-    public double capitalMonneaie(Cryptomonnaie monnaie){
+    public double capitalMonneaie(Cryptomonnaie monnaie) {
         /**
-			FONCTION À IMPLEMENTER
-        **/
+         * FONCTION À IMPLEMENTER
+         **/
 
         return 0;
 
@@ -51,7 +55,7 @@ public class CryptoMarche {
     @Override
     public String toString() {
         String ret = "";
-        for(Portefeuille p : this.portefeuilles){
+        for (Portefeuille p : this.portefeuilles) {
             ret += p.toString() + "\n";
         }
         return ret;
--- src/Cryptomonnaie.java
+++ src/Cryptomonnaie.java
@@ -5,7 +5,7 @@ public class Cryptomonnaie {
 	private double valeurDeJeton; // Imaginons en euros
 
 	public Cryptomonnaie(String nom, double valeurDeJeton) {
-		this.nom           = nom;
+		this.nom = nom;
 		this.valeurDeJeton = valeurDeJeton;
 	}
 
